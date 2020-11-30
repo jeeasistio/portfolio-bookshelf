a65f90deaa03b8b1384256574c15e8fe772d4f5b
jeeasistio <jeeasistio08@gmail.com>
2020-11-30 17:38:08 +0800

    add book functionality added

---
+++ pages/librarian/add-book.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { HFLayout } from '@components/Layout.js'
+import AddBook from '@components/AddBook'
+import { Container } from '@material-ui/core'
+
+const AddBookPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Add Book</title>
+      </Head>
+      <HFLayout>
+        <Container maxWidth="lg">
+          <AddBook />
+        </Container>
+      </HFLayout>
+    </div>
+  )
+}
+
+export default AddBookPage;
